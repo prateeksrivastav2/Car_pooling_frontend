a8faad73192ab631f87df688504528142a0ac06e
Suyash Rawat <rawatsuyash78@gmail.com>
2024-02-27 23:22:42 +0530

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="/">
+          Navbar
+        </Link>
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <Link className="nav-link" to="/">
+              Home 
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/home">
+              second home
+            </Link>
+          </li>
+          </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
