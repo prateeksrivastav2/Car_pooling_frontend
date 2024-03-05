5a9f20c8bc6050586bcf40af4aa4f71d267fb1da
prateeksrivastav2 <prateeksrivastav703@gmail.com>
2024-03-05 23:40:27 +0530

    dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,15 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import ListRides from './ListRides';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Import FontAwesomeIcon
+import { faPlusSquare } from '@fortawesome/free-solid-svg-icons'; // Import the faPlusSquare icon
 import "../styles/Dashboard.css";
 
 const Dashboard = () => {
     const navigate = useNavigate(); // Use useNavigate hook to get the navigate function
+    const handelclick=()=>{
+        navigate('/create-ride')
+    }
 
     useEffect(() => {
         if (!localStorage.getItem('token')) {
@@ -23,6 +28,9 @@ const Dashboard = () => {
                 <div className="col-md-4">
                     <div className="ride-list-box">
                         <ListRides />
+                    </div>
+                    <div className="icon-container">
+                        <FontAwesomeIcon className='text-info' icon={faPlusSquare} size='4x' style={{marginLeft:'1vw'}} onClick={handelclick}/> {/* Use FontAwesomeIcon with faPlusSquare icon */}
                     </div>
                 </div>
             </div>
